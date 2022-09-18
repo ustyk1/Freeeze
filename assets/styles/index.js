d3e4453712d6c9a945e962864b9d4d8663b86237
ustyk1 <eugenia.ustyk@gmail.com>
2022-09-18 14:21:22 +0300

    add bot for contact page form

---
+++ assets/styles/index.js
@@ -0,0 +1,17 @@
+document.querySelector('#btn_inquiry_form').addEventListener('click', function () {
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const subject = document.querySelector('#subject').value;
+    const feedback = document.querySelector('#feedback').value;
+
+    fetch(`https://api.telegram.org/bot5626397760:AAH6sTGYNFvaXESwvLjintDmRhOm0Tc3Om4/sendMessage?chat_id=-542915642&text=Name:+${name}%0AEmail:+${email}%0ASubject:+${subject}%0AFeedback:+${feedback}`).then((res) => {
+        return res;
+    }).catch((err) => {
+
+    });
+});
+
+document.querySelector('#hamburger-toggle').addEventListener('click', () => {
+    document.querySelector('body').classList.toggle('stopScroll');
+    document.querySelector('html').classList.toggle('stopScroll')
+});
